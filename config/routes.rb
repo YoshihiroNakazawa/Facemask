0cee448bb6523ae81820050b2dc72da014823a37
Yoshihiro Nakazawa <y_nkzw@nifty.ne.jp>
2017-01-09 06:01:38 +0000

    トピックへのコメント投稿機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :topics
+  resources :topics do
+    resources :comments
+  end
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {
     registrations: "users/registrations",
