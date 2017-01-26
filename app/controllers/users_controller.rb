a1247266ceaee00e3b084b1e0dcaac98dacabedb
Yoshihiro Nakazawa <y_nkzw@nifty.ne.jp>
2017-01-26 07:10:52 +0000

    user#show画面レイアウト修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,9 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    @users = @users.where( "name LIKE ?", "%#{params[:user_find_form][:name]}%" ) if params[:user_find_form][:name].present?
+    if params[:user_find_form].present?
+      @users = @users.where( "name LIKE ?", "%#{params[:user_find_form][:name]}%" ) if params[:user_find_form][:name].present?
+    end
   end
 
   def show
