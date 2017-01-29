dc1fdd42405796c7286f5e8cac8bbe1bc261b63a
Yoshihiro Nakazawa <y_nkzw@nifty.ne.jp>
2017-01-29 05:36:13 +0000

    user一覧でのクエリ発行を抑止

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,22 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all.includes()
     if params[:user_find_form].present?
-      @users = @users.where( "name LIKE ?", "%#{params[:user_find_form][:name]}%" ) if params[:user_find_form][:name].present?
+      @users = User.find_by_sql(["SELECT users.*,
+        CASE WHEN A.id IS NULL THEN FALSE ELSE TRUE END AS follower,
+        CASE WHEN B.id IS NULL THEN FALSE ELSE TRUE END AS followed
+        FROM users
+        LEFT OUTER JOIN relationships AS A ON (A.follower_id=users.id AND A.followed_id=?)
+        LEFT OUTER JOIN relationships AS B ON (B.followed_id=users.id AND B.follower_id=?)
+        WHERE users.id<>? AND name LIKE ?",current_user.id,current_user.id,current_user.id,"%#{params[:user_find_form][:name]}%"])
+    else
+      @users = User.find_by_sql(["SELECT users.*,
+        CASE WHEN A.id IS NULL THEN FALSE ELSE TRUE END AS follower,
+        CASE WHEN B.id IS NULL THEN FALSE ELSE TRUE END AS followed
+        FROM users
+        LEFT OUTER JOIN relationships AS A ON (A.follower_id=users.id AND A.followed_id=?)
+        LEFT OUTER JOIN relationships AS B ON (B.followed_id=users.id AND B.follower_id=?)
+        WHERE users.id<>?",current_user.id,current_user.id,current_user.id])
     end
   end
 
